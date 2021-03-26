bf41f558a59e1f1a4f6f68d147320379367e7b47
bija510 <error.zxz@gmail.com>
2021-03-25 23:29:31 -0400

    change project name & test push

--- specflow-selenium-c-sharp/Features/HooksAndScopeBindingFF.feature.cs
+++ specflow-selenium-c-sharp/Features/HooksAndScopeBindingFF.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace CsharpSpecflowFW.Features
+namespace specflow_selenium_c_sharp.Features
 {
     using TechTalk.SpecFlow;
     using System;
